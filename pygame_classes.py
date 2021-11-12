b267296ee32f6af436fb08624454d02278cc3458
AntonioRochaAZ <80586458+AntonioRochaAZ@users.noreply.github.com>
2021-11-11 21:34:05 -0300

    Improved PyGame window
    Added real time model prediction to the PyGame code. Not all assets are ready

---
+++ pygame_classes.py
@@ -0,0 +1,41 @@
+# https://pythonprogramming.altervista.org/buttons-in-pygame/
+import pygame
+pygame.font.init()
+
+class Button:
+    """Create a button, then blit the surface in the while loop"""
+
+    def __init__(self, text, pos, size, font, bg="black", feedback=""):
+        self.x, self.y = pos
+        self.size = size
+        self.font = pygame.font.SysFont("timesnewroman", font)
+        if feedback == "":
+            self.feedback = "text"
+        else:
+            self.feedback = feedback
+        self.bg = bg
+        self.change_text(text, bg)
+
+    def change_text(self, text, bg: str = None):
+        """Change the text whe you click"""
+        if bg is None:
+            bg = self.bg
+        self.text = self.font.render(text, 1, pygame.Color("black"))
+        self.surface = pygame.Surface(self.size)
+        self.text_size = self.text.get_size()
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (
+            (self.size[0] - self.text_size[0]) / 2,
+            (self.size[1] - self.text_size[1]) / 2
+        ))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def show(self, screen: pygame.display, button: 'Button'):
+        screen.blit(button.surface, (self.x, self.y))
+
+    def click(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, bg="red")
--- pygametests.py
+++ pygametests.py
@@ -4,8 +4,9 @@
 # Setup Python ----------------------------------------------- #
 import pygame, sys
 from datasets import *
+from pygame_classes import Button
 
-with open('.//_Reports/TwoLayerTestNew/best_model.pkl', 'rb') as f:
+with open('.//_Reports/TwoLayerTest100 - 2021-11-11 16_15_22.806234/small_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 resize = transforms.Resize((32, 32))
@@ -14,62 +15,69 @@ resize = transforms.Resize((32, 32))
 mainClock = pygame.time.Clock()
 from pygame.locals import *
 pygame.init()
-pygame.display.set_caption('game base')
-screen = pygame.display.set_mode((640, 640), 0, 32)
 
-# img = pygame.image.load('pic.png').convert()
+# Drawing variables:
+radius = 30
+extra = 0.1 * radius
 
-offset = [0, 0]
+# Display layers:
+pygame.display.set_caption('Drawing Board')
+screen = pygame.display.set_mode((1280, 640), 0, 32)    # Base screen
+drawing_board = pygame.Surface((640, 640))              # Drawing board
+layer = pygame.Surface((640, 640))      # A layer on top for the mouse
+layer.set_colorkey((255, 255, 255))     # Which is transparent to white colour
 
+# Button initialization:
+button1 = Button('...', (810, 70), (300, 100), font=30, bg="gray", feedback="")
+button2 = Button('...', (810, 270), (300, 100), font=30, bg="gray", feedback="")
+button3 = Button('...', (810, 470), (300, 100), font=30, bg="gray", feedback="")
+
+# Runtime variables (related to mouse clicking):
 clicking = False
 right_clicking = False
 middle_click = False
+
+# Set of points to be drawn:
 draw_set = set()
 
+# Function for calculating the distance between two points (for erasing).
 def distance(point1, point2):
-
     dx2 = (point1[0] - point2[0])**2
     dy2 = (point1[1] - point2[1])**2
-
     r = (dx2+dy2)**(0.5)
-
     return r
-
 
 # Loop ------------------------------------------------------- #
 while True:
 
     # Background --------------------------------------------- #
-    screen.fill((255, 255, 255))
+    screen.fill((116, 125, 207))
+    drawing_board.fill((255, 255, 255))
+    layer.fill((255, 255, 255))
 
-    mx, my = pygame.mouse.get_pos()
-    radius = 30
+    loc = pygame.mouse.get_pos()
 
-    rot = 0
-    loc = [mx, my]
+    # loc = [mx, my]
     if clicking:
         draw_set.add((loc[0], loc[1]))
     if right_clicking:
         remove_set = set()
         for point in draw_set:
-            if distance(loc, point) < radius+5:
+            if distance(loc, point) < radius+extra:
                 remove_set.add(point)
         draw_set = draw_set - remove_set
     if middle_click:
         pass
-    # screen.blit(pygame.transform.rotate(img, rot), (loc[0] + offset[0], loc[1] + offset[1]))
     for point in draw_set:
         # pygame.draw.circle(screen, (0, 0, 0), (point[0], point[1]), radius)
-        pygame.draw.rect(screen, (0, 0, 0),
+        pygame.draw.rect(drawing_board, (0, 0, 0),
                          pygame.Rect((point[0]-radius/2, point[1]-radius/2),
                                      (radius, radius)), width=0)
-
-    # pygame.draw.circle(screen, (0, 0, 0), (loc[0], loc[1]), radius)
-    pygame.draw.rect(screen, (0, 0, 0),
-                     pygame.Rect((loc[0] - radius / 2, loc[1] - radius / 2),
-                                 (radius, radius)), width=0)
     # Buttons ------------------------------------------------ #
     for event in pygame.event.get():
+        button1.click(event)
+        button2.click(event)
+        button3.click(event)
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -93,14 +101,85 @@ while True:
                 clicking = False
             if event.button == 3:
                 right_clicking = False
+    button1.show(screen, button1)
+    button2.show(screen, button2)
+    button3.show(screen, button3)
 
     # Update ------------------------------------------------- #
-    pygame.display.update()
-    pygame.draw.circle(screen, (255, 255, 255), (loc[0], loc[1]), radius)
-    pygame.image.save(screen, ".//_Data/pyimage.jpg")
+    screen.blit(drawing_board, (0,0))
+    # pygame.draw.circle(screen, (0, 0, 0), (loc[0], loc[1]), radius)
+    pygame.draw.rect(layer, (0, 0, 0),
+                     pygame.Rect((loc[0] - radius / 2, loc[1] - radius / 2),
+                                 (radius, radius)), width=0)
+    screen.blit(layer, (0, 0))
+
+    # Model --------------------------------------------------- #
+
+    pygame.image.save(drawing_board, ".//_Data/pyimage.jpg")
+
+    lbl_list = []
+    val_list = []
     img = Image.open(".//_Data/pyimage.jpg")
     tsr = grayscale(img_to_tsr(resize(img))) - 0.5
-    index = int(th.argmax(model(tsr.view(1, 1, 32, 32))))
+    out = model(tsr.view(1, 1, 32, 32)).view(-1)
+    out_copy = out.clone()
+    for i in range(3):
+        idx = int(th.argmax(out))
+        label = HASYv2Dataset.latex_dict[idx]
+        if label.islower():
+            label = "_"+label
+        lbl_list.append(label)
+        val_list.append(out_copy[idx])
+        out[idx] = 0
+    #
+    #
+    # index_2 = int(th.argmax(out))
+    # val_2 = out_copy[index_2]
+    # out[index_2] = 0
+    # index_3 = int(th.argmax(out))
+    # val_3 = out_copy[index_3]
+    # Images (will be updated)
+
+    try:
+        img1 = pygame.image.load(f'.//_Assets/{lbl_list[0]}.png').convert()
+        img1 = pygame.transform.scale(img1, (160, 144))
+        img1.set_colorkey((255, 255, 255))
+        screen.blit(img1, (1120, 50))
+    except FileNotFoundError:
+        pass
+    try:
+        img2 = pygame.image.load(f'.//_Assets/{lbl_list[1]}.png').convert()
+        img2 = pygame.transform.scale(img2, (160, 144))
+        img2.set_colorkey((255, 255, 255))
+        screen.blit(img2, (1120, 250))
+    except FileNotFoundError:
+        pass
+    try:
+        img3 = pygame.image.load(f'.//_Assets/{lbl_list[2]}.png').convert()
+        img3 = pygame.transform.scale(img3, (160, 144))
+        img3.set_colorkey((255, 255, 255))
+        screen.blit(img3, (1120, 450))
+    except FileNotFoundError:
+        pass
+
+    button1.change_text(
+        '{name}: {val:.4g}'.format(
+            name=lbl_list[0],val=val_list[0]))
+    button2.change_text(
+        '{name}: {val:.4g}'.format(
+            name=lbl_list[1], val=val_list[1]))
+    button3.change_text(
+        '{name}: {val:.4g}'.format(
+            name=lbl_list[2], val=val_list[2]))
+    # button3 = Button(
+    #     '{name}: {val:.4g}'.format(name=HASYv2Dataset.latex_dict[index_3], val=val_3),
+    #     (940, 600), (300, 100),
+    #     font=30,
+    #     bg="gray",
+    #     feedback="You clicked me")
+
     # plt.imshow(tsr.view(32, 32).detach())
-    print(index, HASYv2Dataset.latex_dict[index])
+    # print(index, HASYv2Dataset.latex_dict[index])
+
+    pygame.display.update()
     mainClock.tick(60)
